8f1eba441735362a574fc86d5a168e97030a88e7
AlbertoWebDev <{your_email}>
2026-01-20 12:14:11 +0100

    Arrays created

---
+++ EJ_Evaluable_1.php
@@ -0,0 +1,22 @@
+<?php
+
+$carrito = [];
+
+$inventario = [
+    "Electronica" => [
+        "Switch 2" => ["nombre" => "Switch 2", "precio" => 400, "cantidad" => 10],
+        "Iphone 16" => ["nombre" => "Iphone 16", "precio" => 600, "cantidad" => 5],
+        "Auriculares Logitech" => ["nombre" => "Auriculares Logitech", "precio" => 30, "cantidad" => 15]
+    ],
+    "Ropa" => [
+        "Chaqueta" => ["nombre" => "Chaqueta", "precio" => 50, "cantidad" => 4],
+        "Camiseta" => ["nombre" => "Camiseta", "precio" => 20, "cantidad" => 50]
+    ],
+    "Alimentos" => [
+        "Barrita Kitkat" => ["nombre" => "Barrita Kitkat", "precio" => 2, "cantidad" => 100],
+        "Pan de masa madre" => ["nombre" => "Pan de", "precio" => 1.5, "cantidad" => 20]
+    ]
+];
+
+
+?>
