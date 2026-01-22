10014ee59efa2844a85acab44e88cde87bd231e0
AlbertoWebDev <{your_email}>
2026-01-22 08:46:23 +0100

    Inventory visible through the console

--- EJ_Evaluable_1.php
+++ EJ_Evaluable_1.php
@@ -1,7 +1,6 @@
 <?php
 
 $carrito = [];
-// Arrays multidimensionales que serán añadidos al array "carrito"
 $inventario = [
     "Electronica" => [
         "Switch 2" => ["nombre" => "Switch 2", "precio" => 400, "cantidad" => 10],
@@ -19,30 +18,29 @@ $inventario = [
 ];
 
 function agregarAlCarrito($categoria, $producto, $cantidad) {
-
     global $inventario, $carrito;
 
-    // Buscamos el precio del producto dentro del inventario
-    // Entramos en la categoría y luego en el nombre del producto
     $precioUnitario = $inventario[$categoria][$producto]["precio"];
 
-    // Comprobamos si hay suficiente stock
     if ($inventario[$categoria][$producto]["cantidad"] >= $cantidad) {
-        
-        // Lo añadimos al array carrito creado anteriormente
         $carrito[] = [
             "nombre" => $producto,
             "cantidad" => $cantidad,
             "precio" => $precioUnitario
         ];
 
-        // Restamos la cantidad del inventario original para que no se nos acabe en ningún caso
         $inventario[$categoria][$producto]["cantidad"] = $inventario[$categoria][$producto]["cantidad"] - $cantidad;
         
         echo "Añadido: " . $producto . " al carrito.<br>";
-// utilizamos <br> para hacer un salto de linea cada vez que añadamos un producto (breakpoint)
     } else {
         echo "No tenemos suficiente stock de " . $producto . "<br>";
     }
 }
+
+// Agregamos y mostramos por consola
+agregarAlCarrito("Electronica", "Switch 2", 1);
+agregarAlCarrito("Ropa", "Chaqueta", 2);
+
+print_r($inventario["Electronica"]["Switch 2"]);
+print_r($inventario["Ropa"]["Chaqueta"]);
 ?>
